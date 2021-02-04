4234b37b2b783d24b2114ced14dd34cf09613add
github username <github email address>
2021-02-04 01:46:43 -0500

    existential timer

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,27 +1,19 @@
 import './App.css';
 import Counter from "./Counter";
 
-//render in specific html known as JSX
-//javascript code inside of html, looks like html
-//image tag. within img tag set source to variable "logo"
-//can set variable names in jsx 
-//you can only have one root tag for every render file
-//parent to child relationship 
-//our parent: app component
-//we are inputting another component, counter, child, within that
-
 function App() {
   return (
     <div className="App">
       <div>
-     <h1> YOU SUCK, TIME IS RUNNING OUT </h1>
+        <h1> EXISTENTIAL TIMER </h1>
+     <h1>  TIME IS RUNNING OUT </h1>
      <Counter />
      <Counter />
      <Counter />
      <Counter />
      <Counter />
     </div>
-     <h2> AHHHH </h2>
+     <h2> AHHHH! </h2>
     </div>
   );
 }
--- counter/src/Counter.js
+++ counter/src/Counter.js
@@ -1,23 +1,10 @@
 import React, { Component } from "react";
 
-//functionally vs class oriented 
-//lightweight: functionally 
-//high level: class structure 
-//declare class with name of component
-//extended component class, necessary syntax 
-//exporting 
-//state: a way to keep track of instance variables within your component 
-//props: something that will run whenever component is mounted, or created
-//modular way, make our buttons change the state of our component
-//state is being incremented or decrimented 
-//how do we connect the buttons to the functions? Javascript event listeners
-
-
 class Counter extends Component {
     constructor(props){
         super(props)
         this.state = {
-            count: 0
+            count: 100
         };
     }
 
@@ -37,14 +24,26 @@ decriment = () => {
 render() {
     return (
         <div>
-            <p> This is our counter </p>
-            <p>Current Count: {this.state.count} </p>
+            <p> :( </p>
+            <p>Time Left: {this.state.count} </p>
             <button onClick={this.increment}>Increment</button>
             <button onClick={this.decriment}>Decrement</button>
         </div>
     );
   }   
 
+  componentDidMount () {
+      this.myInterval = setInterval(() => {
+          this.setState({
+              count: this.state.count - 1
+          })
+      }, 1000)
+  }
+
+
+componentWillUnmount () {
+    clearInterval(this.myInterval)
+    }
 }
 
 
